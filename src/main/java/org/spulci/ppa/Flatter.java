561fa69135c6ebf2a948b3170ce2f867b34cb090
Simone Pulcini <pulcini@uirnet.it>
2020-12-22 15:59:47 +0100

    refactor(resource): minor signature changes

--- src/main/java/org/spulci/ppa/Flatter.java
+++ src/main/java/org/spulci/ppa/Flatter.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
 import java.util.stream.Collectors;
-public class App{
+public class Flatter{
     public <T> List<T> flattenListOfListsStream(List<List<T>> list) {
         return list.stream()
           .flatMap(Collection::stream)
--- src/test/java/org/spulci/ppa/FlatterTest.java
+++ src/test/java/org/spulci/ppa/FlatterTest.java
@@ -9,15 +9,15 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 
-public class AppTest 
+public class FlatterTest 
 {   
-    static App testApp;
+    static Flatter testApp;
     static List<List<Integer>> listToBeFlattened;
     static Integer [] [] arrayToBeFlattened;
 
     @BeforeAll
     static void init(){
-        testApp = new App();
+        testApp = new Flatter();
         listToBeFlattened = List.of(List.of(1,2,4), List.of(1));
         arrayToBeFlattened = new Integer[][]{{1,2,4},{2}};
     }
@@ -26,13 +26,13 @@ public class AppTest
     @DisplayName("flat a List of <T>")
     void whenNestedListOfIntegerIsFlattened(){
         List<?> flattedList = testApp.flattenListOfListsStream(listToBeFlattened);
-        assertEquals(flattedList.size(), 4);
+        assertEquals(4, flattedList.size());
     }
 
     @Test
     @DisplayName("flat an Array of Integer")
     void whenNestedArrayOfIntegerIsFlattened(){
         Integer[] flattedList = testApp.flattenArrayOfArrayStream(arrayToBeFlattened);
-        assertEquals(flattedList.length, 4);
+        assertEquals(4, flattedList.length);
     }
 }
